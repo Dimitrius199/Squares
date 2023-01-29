59a58c2e071ccf876bfefc2548dd0b0efff7664c
Dmitrii Selin <120986503+Dimitrius199@users.noreply.github.com>
2023-01-30 00:49:31 +0500

    initial commit

--- src/main/java/ru/netology/border/bordersofsquares/services/SQRService.java
+++ src/main/java/ru/netology/border/bordersofsquares/services/SQRService.java
@@ -2,10 +2,10 @@ package ru.netology.border.bordersofsquares.services;
 
 public class SQRService {
 
-    public int calcSQR (int a, int b) {
+    public int calcSQR (int boundlow, int boundhigh) {
         int counter = 0;
         for (int i = 10; i < 99; i++) {
-            if ((i * i) >= a && (i * i) <= b) {
+            if ((i * i) >= boundlow && (i * i) <= boundhigh) {
                 counter++;
             }
         }
